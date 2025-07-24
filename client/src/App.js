b6188eb5851db3150836c0e22dc25df3b9aa8cbf
prakharph20b010 <ph20b010@smail.iitm.ac.in>
2025-07-24 14:37:41 +0530

    inital commit

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [msg, setMsg] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/hello')
+      .then(res => res.json())
+      .then(data => setMsg(data.message));
+  }, []);
+
+  return (
+    <div>
+      <h1>My Business Website</h1>
+      <p>{msg}</p>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ server/index.js
@@ -0,0 +1,17 @@
+// server/index.js
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from Node.js backend!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
